90c225d7566e280ee3421b756ff3b6b1e6e3c8cf
rapha1004 <106275964+rapha1004@users.noreply.github.com>
2023-06-11 15:07:47 +0200

    analize

--- script.js
+++ script.js
@@ -47,3 +47,16 @@ btncopy.addEventListener('click', () => {
         alert("mon pseudo a était copier!"); 
     }, 200);
 })
+
+import { Analytics } from '@vercel/analytics/react';
+ 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+ 
+export default MyApp;
