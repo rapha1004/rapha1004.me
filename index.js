96d66df17ce679a153e8d67c2095ca6703ba8bd2
rapha1004 <106275964+rapha1004@users.noreply.github.com>
2024-10-16 07:46:31 +0000

    r

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
