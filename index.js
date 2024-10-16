f860abe57603d3702eafc81ada42e9d1830151f4
rapha1004 <106275964+rapha1004@users.noreply.github.com>
2024-10-16 09:02:48 +0000

    init

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const routes = require('./routes/routes');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Accueil' });
-});
-
+app.use('/', routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
