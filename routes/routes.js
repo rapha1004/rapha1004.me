fe7da18189db4951944c7f6764dc556ff27b1533
rapha1004 <106275964+rapha1004@users.noreply.github.com>
2024-10-18 12:22:07 +0000

    fix arch

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
 app.get('/*', (req, res) => {
     res.render('404');
 });
