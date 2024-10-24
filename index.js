20ef87c0783139c3018dfa0a9f04c97714f0334a
rapha1004 <106275964+rapha1004@users.noreply.github.com>
2024-10-24 22:25:04 +0000

    blalblablalblalbla

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const routes = require('./routes/routes');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/*', (req, res) => {
+    res.render('404');
+});
+
+const PORT = process.env.PORT || 2432;
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
--- todo.md
+++ todo.md
@@ -1,1 +1,2 @@
 ecrire les ptn de texte et pas les generer avec chatgpt
+regler problème footer dans about.ejs (si c'est trop chiant mettre le footer que sur home)
